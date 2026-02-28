2f026db79ec3324e7fec346de66cba10e3153b83
toastkidjp <10fmi13@gmail.com>
2026-02-28 12:25:09 +0900

    refactor Fix incorrect unit test.

--- app/src/test/java/jp/toastkid/chat/domain/model/GenerativeAiModelTest.kt
+++ app/src/test/java/jp/toastkid/chat/domain/model/GenerativeAiModelTest.kt
@@ -1,5 +1,6 @@
 package jp.toastkid.chat.domain.model
 
+import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Test
 
@@ -7,11 +8,10 @@ class GenerativeAiModelTest {
 
     @Test
     fun image() {
-        assertTrue(GenerativeAiModel.GEMINI_2_0_FLASH_IMAGE.image())
+        assertFalse(GenerativeAiModel.GEMINI_2_5_FLASH.image())
 
         assertTrue(
             GenerativeAiModel.entries
-                .filter { it != GenerativeAiModel.GEMINI_2_0_FLASH_IMAGE }
                 .none(GenerativeAiModel::image)
         )
     }
